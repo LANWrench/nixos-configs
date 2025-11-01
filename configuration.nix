add67ca460299e69afec521ac5b8e46fd061a71e
Michael <5728708+LANWrench@users.noreply.github.com>
2025-10-31 19:20:55 -0500

    first commit

---
+++ configuration.nix
@@ -0,0 +1,70 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./programs.nix
+    ];
+
+########################################
+#### SYSTEM ####
+########################################
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+#   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+########################################
+#### SERVICES ####
+########################################
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  services.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+
+  services.getty.autologinUser = "michael";
+
+########################################
+#### NETWORKING ####
+########################################
+  networking.hostName = "nixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  programs.hyprland = {
+	enable = true;
+	xwayland.enable = true;
+  };
+
+########################################
+#### USERS ####
+########################################
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.michael= {
+    isNormalUser = true;
+    extraGroups = [ 
+    	"wheel"
+    	"networkmanager" 
+    ]; 
+  };
+}
---
+++ programs.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+
+{
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+	kitty
+	waybar
+	git
+	hyprpaper
+    fzf
+  ];
+
+  programs.kitty.enable = true;
+  programs.hyprland.enable = true;
+
+  programs.git = {
+    enable = true;
+    userEmail = "5728708+LANWrench@users.noreply.github.com";
+    userName = "Michael";
+  };
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    defaultEditor = true;
+    extraConfig = ''
+      filetype plugin indent on
+      set expandtab
+      set shiftwidth=2
+      set softtabstop=2
+      set tabstop=2
+      set number
+      set relativenumber
+      set smartindent
+      set showmatch
+      set backspace=indent,eol,start
+      syntax on
+    '';
+  };
+}
